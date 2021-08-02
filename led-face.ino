10129330bc64d0087892addf06561fa82ed9cb96
cburtsev <cyril.burtsev@mail.ru>
2021-08-02 22:42:28 +0300

    use SSD1306 display connected to I2C
    Signed-off-by: cburtsev <cyril.burtsev@mail.ru>

--- led-face.ino
+++ led-face.ino
@@ -1,44 +1,60 @@
 /*
- * Arduino MAX7219 demo
+ * Arduino SSD1306 I2C demo
  * based on
- *   https://robotclass.ru/tutorials/arduino-matrix-8x8-max7219/
+ *   https://github.com/adafruit/Adafruit_SSD1306/blob/master/examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
  */
 
-#include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Max72xxPanel.h>
+#include <Adafruit_SSD1306.h>
 
-int pinCS = 9;
-int numberOfHorizontalDisplays = 1; // количество матриц по-горизонтали
-int numberOfVerticalDisplays = 1; // количество матриц по-вертикали
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3c ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-    matrix.setIntensity(4); // яркость от 0 до 15
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Clear the buffer
+  display.clearDisplay();
+
 }
 
 void loop() {
-    matrix.drawPixel(1, 2, HIGH); // зажигаем пиксель с координатами {3,4} 
-    matrix.drawPixel(2, 3, HIGH); // зажигаем пиксель с координатами
-    matrix.drawPixel(2, 2, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(1, 3, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(5, 3, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(6, 3, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(6, 2, HIGH); // зажигаем пиксель с координатами
-    matrix.drawPixel(5, 2, HIGH); // зажигаем пиксель с координатами
-    matrix.drawPixel(1, 5, HIGH); // зажигаем пиксель с координатами
-    matrix.drawPixel(1, 6, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(2, 6, HIGH); // зажигаем пиксель с координатами
-    matrix.drawPixel(3, 6, HIGH); // зажигаем пиксель с координатами
-    matrix.drawPixel(4, 6, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(5, 6, HIGH); // зажигаем пиксель с координатами 
-    matrix.drawPixel(6, 6, HIGH); // зажигаем пиксель с координатами  
-    matrix.drawPixel(6, 5, HIGH); // зажигаем пиксель с координатами    
-    matrix.write(); // вывод всех пикселей на матрицу
+    display.drawPixel(1, 2, HIGH); // зажигаем пиксель с координатами {3,4} 
+    display.drawPixel(2, 3, HIGH); // зажигаем пиксель с координатами
+    display.drawPixel(2, 2, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(1, 3, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(5, 3, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(6, 3, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(6, 2, HIGH); // зажигаем пиксель с координатами
+    display.drawPixel(5, 2, HIGH); // зажигаем пиксель с координатами
+    display.drawPixel(1, 5, HIGH); // зажигаем пиксель с координатами
+    display.drawPixel(1, 6, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(2, 6, HIGH); // зажигаем пиксель с координатами
+    display.drawPixel(3, 6, HIGH); // зажигаем пиксель с координатами
+    display.drawPixel(4, 6, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(5, 6, HIGH); // зажигаем пиксель с координатами 
+    display.drawPixel(6, 6, HIGH); // зажигаем пиксель с координатами  
+    display.drawPixel(6, 5, HIGH); // зажигаем пиксель с координатами    
+    display.display(); // вывод всех пикселей на матрицу
     delay(5000);
 
-    matrix.fillScreen(LOW);
-    matrix.write();
+    display.clearDisplay();
+    display.display(); // вывод всех пикселей на матрицу
     delay(300);
 }
